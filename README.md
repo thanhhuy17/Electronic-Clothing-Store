e076e3913f906420897e5c10da4166e992cbf616
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-08-05 23:50:24 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install
+
+1. npm install express
+2. npm install mongoose
+3. npm install cors
+4. npm install dotenv
+5. npm install bcrypt
+6. npm install cookie-parser
+7. npm install jsonwebtoken
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.listen(8000, function () {
+  console.log("Server is Running...");
+});
