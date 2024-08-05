3db985a7756eb4df534b250f9b90fe6924c1ced0
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-08-06 00:29:56 +0700

    connect to MongoDB

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+dotenv.config();
 const app = express();
+
+// Cách chạy MongoDB mới
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected To Mongoose DB!");
+  } catch (err) {
+    console.error("Error Connecting To MongoDB", err);
+  }
+}
+
+connectToMongoDB();
+// mongoose.connect(process.env.MONGODB_URL, () => {
+//   console.log("Connected To Mongoose DB!");
+// });
 
 app.use(cors());
 app.use(cookieParser());
@@ -12,5 +28,3 @@ app.use(express.json());
 app.listen(8000, function () {
   console.log("Server is Running...");
 });
-
-
