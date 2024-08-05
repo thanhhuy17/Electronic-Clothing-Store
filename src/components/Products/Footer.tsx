6b626f7db9d3284416c3c9bdd4029ea40f879369
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-08-06 00:44:38 +0700

    a

--- src/components/Products/Footer.tsx
+++ src/components/Products/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="mx-20 text-neutral-700">Design by HuyNguyen </div>;
+  return <div className="mx-20 text-neutral-700">Design by HuyNguyen</div>;
 };
 
 export default Footer;
